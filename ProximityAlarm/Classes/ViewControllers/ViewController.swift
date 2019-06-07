29c63551de254e04eee72ddb8271187fb5bf770c
Manuel Olmos Gil <manuolmos88@gmail.com>
2019-06-06 23:08:19 -0300

    Project structure and add gitignore

---
+++ ProximityAlarm/Classes/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProximityAlarm
+//
+//  Created by Manuel Olmos Gil on 06/06/2019.
+//  Copyright © 2019 Manuel Olmos Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
