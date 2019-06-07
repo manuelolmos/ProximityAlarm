c99b1f9852e4757606adfe34c6352e9ce367bacc
Manuel Olmos Gil <manuolmos88@gmail.com>
2019-06-07 00:30:14 -0300

    Connect components to set the distance when to trigger the alarm

---
+++ ProximityAlarm/Classes/Models/AppConfig.swift
@@ -0,0 +1,16 @@
+
+//
+//  AppConfig.swift
+//  ProximityAlarm
+//
+//  Created by Manuel Olmos Gil on 07/06/2019.
+//  Copyright © 2019 Manuel Olmos Gil. All rights reserved.
+//
+
+import Foundation
+
+class AppConfig: NSObject {
+    static let maxDistanceToTrigger: Float = 1000.0
+    static let minDistanceToTrigger: Float = 0.0
+
+}
--- ProximityAlarm/Classes/ViewControllers/ViewController.swift
+++ ProximityAlarm/Classes/ViewControllers/ViewController.swift
@@ -12,15 +12,33 @@ import MapKit
 class ViewController: UIViewController, LocationDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var finalPositionSliderLabel: UILabel!
+    @IBOutlet weak var initPositionSliderLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+
+    private let distanceToCover: Float = AppConfig.maxDistanceToTrigger
+    private var distanceToTrigger: Float = 0.0
     private var locationManager = LocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = LocationManager()
         locationManager.delegate = self
+        setupUIComponents()
     }
 
     func locationDidUpdate(region: MKCoordinateRegion) {
         mapView.setRegion(region, animated: true)
+    }
+
+    private func setupUIComponents() {
+        distanceLabel.text = "When to trigger alarm: \(distanceToCover*0.5)m"
+        initPositionSliderLabel.text = "\(AppConfig.minDistanceToTrigger)m"
+        finalPositionSliderLabel.text = "\(distanceToCover)m"
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        distanceToTrigger = round(sender.value*distanceToCover)
+        distanceLabel.text = "When to trigger alarm: \(distanceToTrigger)m"
     }
 }
